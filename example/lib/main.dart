82d1dd21cd7feb26f1bef34a7ba2b72aa776b2d3
Guido Theelen <guido@kabisa.nl>
2022-07-10 18:49:19 +0200

    add example to project

---
+++ example/lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:number_selector/number_selector.dart';
+
+void main() {
+  runApp(const NumberSelectorApp());
+}
+
+class NumberSelectorApp extends StatelessWidget {
+  const NumberSelectorApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Number Selector App'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Text('Number selector with min and max'),
+            ),
+            NumberSelector(
+              current: 47,
+              min: 1,
+              max: 100,
+              onUpdate: (val) {
+                print('onUpdate: $val');
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Text('Number selector without min and max'),
+            ),
+            NumberSelector(
+              current: 47,
+              onUpdate: (val) {
+                print('onUpdate: $val');
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Text('Basic number selector'),
+            ),
+            NumberSelector(
+              height: 50,
+              width: 180,
+              current: 47,
+              backgroundColor: Colors.transparent,
+              incrementIcon: Icons.add,
+              decrementIcon: Icons.remove,
+              showSuffix: false,
+              hasBorder: false,
+              hasDividers: false,
+              hasCenteredText: true,
+              onUpdate: (val) {
+                print(val);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
